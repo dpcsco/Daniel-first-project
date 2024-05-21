7e854358d2326806789f64e7053d2f885bf43dc9
dpcsco <dpcsco@gmail.com>
2024-05-20 21:55:18 -0600

    Create hangman.py

---
+++ hangman.py
@@ -0,0 +1,7 @@
+echo "# Daniel-first-project" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/dpcsco/Daniel-first-project.git
+git push -u origin main
